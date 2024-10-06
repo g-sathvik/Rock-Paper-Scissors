1753c0a5c0af5f5addd0d12f6237491be3702579
Gampa Sathvik <120566003+g-sathvik@users.noreply.github.com>
2024-10-06 23:00:05 +0530

    Add basic HTML UI

--- script.js
+++ script.js
@@ -40,67 +40,96 @@ function getHumanChoice()
     }
 }
 
-function playRound()
+function playRound(humanChoice)
 {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    let message = "";
-    if(humanChoice === computerChoice)
+    if(computerScore < 5 && humanScore < 5)
     {
-        message+=(`You both chose ${humanChoice}!. Nobody won.`)
-    }
-    else
-    {
-        if(humanChoice == "rock")
+        const computerChoice = getComputerChoice();
+        let message = "";
+        if(humanChoice === computerChoice)
         {
-            if(computerChoice == "paper")
-            {
-                message+=(`You lost! Paper beats Rock`);
-                computerScore++;
-            }
-            else
-            {
-                message+=(`You won! Rock beats Scissors`);
-                humanScore++;
-            }
-        }
-        else if(humanChoice == "paper")
-        {
-            if(computerChoice == "scissors")
-            {
-                message+=(`You lost! Scissors beats Paper`);
-                computerScore++;
-            }
-            else
-            {
-                message+=(`You won! Paper beats Rock`);
-                humanScore++;
-            }
+            message+=(`You both chose ${humanChoice}!. Nobody won.`)
         }
         else
         {
-            if(computerChoice == "rock")
+            if(humanChoice == "rock")
             {
-                message+=(`You lost! Rock beats Scissors`);
-                computerScore++;
+                if(computerChoice == "paper")
+                {
+                    message+=(`You lost! Paper beats Rock`);
+                    computerScore++;
+                }
+                else
+                {
+                    message+=(`You won! Rock beats Scissors`);
+                    humanScore++;
+                }
+            }
+            else if(humanChoice == "paper")
+            {
+                if(computerChoice == "scissors")
+                {
+                    message+=(`You lost! Scissors beats Paper`);
+                    computerScore++;
+                }
+                else
+                {
+                    message+=(`You won! Paper beats Rock`);
+                    humanScore++;
+                }
             }
             else
             {
-                message+=(`You won! Scissors beats Paper`);
-                humanScore++;
-            }           
+                if(computerChoice == "rock")
+                {
+                    message+=(`You lost! Rock beats Scissors`);
+                    computerScore++;
+                }
+                else
+                {
+                    message+=(`You won! Scissors beats Paper`);
+                    humanScore++;
+                }           
+            }
+
         }
+        let messageHTML = document.createElement("p");
+        messageHTML.textContent = message;
+        let results = document.querySelector("#results");
+        results.appendChild(messageHTML);
+        let humanScoreHTML = document.querySelector("#hs");
+        let computerScoreHTML = document.querySelector("#cs");
+        humanScoreHTML.textContent = String(humanScore);
+        computerScoreHTML.textContent = String(computerScore);
+        if(humanScore === 5)
+        {
+            let announceHTML = document.createElement("h1");
+            announceHTML.textContent = `You won!!`;
+            let announce = document.querySelector('#announce');
+            announce.appendChild(announceHTML);
+        }
+        if(computerScore === 5)
+            {
+                let announceHTML = document.createElement("h1");
+                announceHTML.textContent = `You lost!!`;
+                let announce = document.querySelector('#announce');
+                announce.appendChild(announceHTML);
+            }
     }
-    console.log(message);
 }
 
-function playGame()
-{
-    for(let i = 0;i < 5;i++)
-    {
-        playRound();
+let userInput = document.querySelector(".input");
+userInput.addEventListener('click', (event) => {
+    let tar = event.target;
+    switch(tar.className) {
+        case 'rock':
+            playRound('rock');
+            break;
+        case 'paper':
+            playRound('paper');
+            break;
+        default:
+            playRound('scissors');
+            break;
     }
-    console.log(`You won ${humanScore} times. Computer won ${computerScore} number of times.`)
-}
-
-playGame();
+})
